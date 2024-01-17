7e548af94b5a48d91d8b1a80987c67c59ca511d7
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2024-01-17 21:51:37 +0530

    📦 NEW : Add RedwoodJS dependencies and create new pages and components

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,11 +7,20 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route } from '@redwoodjs/router'
+
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Route path="/home" page={HomePage} name="home" />
+      <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
