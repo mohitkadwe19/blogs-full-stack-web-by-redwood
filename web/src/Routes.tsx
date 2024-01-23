1a36ab1be33496b86983e8483fea26b963e1a698
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2024-01-23 23:06:47 +0530

    Reorder routes and add Home link in BlogLayout

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -15,17 +15,17 @@ import BlogLayout from 'src/layouts/BlogLayout'
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={BlogLayout}>
-      <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
-      <Route path="/about" page={AboutPage} name="about" />
-      <Route path="/contact" page={ContactPage} name="contact" />
-      <Route path="/" page={HomePage} name="home" />
-      </Set>
       <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
         <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
         <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
+      <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+        <Route path="/contact" page={ContactPage} name="contact" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
