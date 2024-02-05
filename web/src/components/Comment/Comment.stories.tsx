e85c127f628194af134cb28a99f5bdf4e8f0c720
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2024-02-05 22:06:53 +0530

    Update RedwoodJS dependencies to version 6.6.4

---
+++ web/src/components/Comment/Comment.stories.tsx
@@ -0,0 +1,30 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import Comment from './Comment'
+
+export const generated = () => {
+  return (
+    <Comment
+      comment={{
+        name: 'Rob Cameron',
+        body: 'This is the first comment!',
+        createdAt: '2020-01-01T12:34:56Z',
+      }}
+    />
+  )
+}
+
+export default {
+  title: 'Components/Comment',
+  component: Comment,
+}
