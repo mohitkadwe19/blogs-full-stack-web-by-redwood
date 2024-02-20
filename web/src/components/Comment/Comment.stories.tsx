bdcc703947f951add03f998836d7e53f7b862b25
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2024-02-20 22:12:19 +0530

    📦 NEW: Refactor Comment component and add moderator view

--- web/src/components/Comment/Comment.stories.tsx
+++ web/src/components/Comment/Comment.stories.tsx
@@ -12,19 +12,38 @@
 
 import Comment from './Comment'
 
-export const generated = () => {
+export const defaultView = () => {
   return (
     <Comment
       comment={{
+        id: 1,
         name: 'Rob Cameron',
         body: 'This is the first comment!',
         createdAt: '2020-01-01T12:34:56Z',
+        postId: 1,
       }}
     />
   )
 }
 
-export default {
-  title: 'Components/Comment',
-  component: Comment,
+export const moderatorView = () => {
+  mockCurrentUser({
+    id: 1,
+    email: 'moderator@moderator.com',
+    roles: 'moderator',
+  })
+  
+  return (
+    <Comment
+      comment={{
+        id: 1,
+        name: 'Rob Cameron',
+        body: 'This is the first comment!',
+        createdAt: '2020-01-01T12:34:56Z',
+        postId: 1,
+      }}
+    />
+  )
 }
+
+export default { title: 'Components/Comment' }
