48a9f5493cce4c5d8e8be7986585f097295b980a
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2024-02-22 22:12:26 +0530

    Add imports and delete extra space in comments.test.ts

--- api/src/services/comments/comments.test.ts
+++ api/src/services/comments/comments.test.ts
@@ -1,5 +1,7 @@
-import { comments, createComment } from './comments'
-import type { StandardScenario, PostOnlyScenario } from './comments.scenarios'
+import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
+
+import { comments, createComment, deleteComment } from './comments'
+import type { PostOnlyScenario, StandardScenario } from './comments.scenarios'
 
 describe('comments', () => {
   scenario('returns all comments', async (scenario: StandardScenario) => {
@@ -26,6 +28,50 @@ describe('comments', () => {
       expect(comment.body).toEqual('What is your favorite tree bark?')
       expect(comment.postId).toEqual(scenario.post.bark.id)
       expect(comment.createdAt).not.toEqual(null)
+    }
+  )
+  scenario(
+    'allows a moderator to delete a comment',
+    async (scenario: StandardScenario) => {
+      mockCurrentUser({
+        roles: 'moderator',
+        id: 1,
+        email: 'moderator@moderator.com',
+      })
+
+      const comment = await deleteComment({
+        id: scenario.comment.jane.id,
+      })
+      expect(comment.id).toEqual(scenario.comment.jane.id)
+
+      const result = await comments({ postId: scenario.comment.jane.postId })
+      expect(result.length).toEqual(0)
+    }
+  )
+
+  scenario(
+    'does not allow a non-moderator to delete a comment',
+    async (scenario: StandardScenario) => {
+      mockCurrentUser({ roles: 'user', id: 1, email: 'user@user.com' })
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id,
+        })
+      ).toThrow(ForbiddenError)
+    }
+  )
+
+  scenario(
+    'does not allow a logged out user to delete a comment',
+    async (scenario: StandardScenario) => {
+      mockCurrentUser(null)
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id,
+        })
+      ).toThrow(AuthenticationError)
     }
   )
 })
